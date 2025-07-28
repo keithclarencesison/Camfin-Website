6d6740a55c0afc4136f1a0bc3acc51763f6b265c
Keith Clarence Sison <sisonkeithclarence@gmail.com>
2025-07-28 09:56:58 +0800

    initial commit
    commit changes

--- resources/views/sections/loan-products.blade.php
+++ resources/views/sections/loan-products.blade.php
@@ -1,12 +1,71 @@
 <div class="flex flex-col justify-center flex-wrap shrink">
-    <div class="product-header">
+    <div class="product-header mb-20">
         <h1 class="text-center text-3xl text-[#538FD6] font-bold">Our Loan Products</h1>
         <p class="text-center">We offer reliable and flexible financing solutions to help you achieve life’s biggest milestones with confidence and ease.</p>
     </div>
     
 
-    <div class="product-section ">
+    <div class="product-section w-full bg-gray-100">
+        
+        <div class="product1 flex flex-wrap gap-20 justify-center">
+            <div class="product1-description w-[500px] flex flex-col justify-center gap-5">
+                <h1 class="font-bold text-[#538FD6] text-5xl text-center">Sangla OR/CR</h1>
+                <p class="italic text-lg font-bold text-center">Kailangan mo ba ng agarang cash pero ayaw mong iwan ang sasakyan mo?</p>
+                <p class=" text-[14px] text-center"> Sa aming Sangla OR/CR service, puwede mong gamitin ang Official Receipt at Certificate of Registration 
+                    ng iyong sasakyan bilang collateral — at ikaw pa rin ang gagamit ng sasakyan. Mabilis ang proseso, flexible ang terms, at
+                     mababa ang interest rate. Perfect ito para sa mga biglaang pangangailangan o dagdag puhunan sa negosyo.
+                </p>
+                <button class="btn w-1/2 self-center">Apply Now</button>
+            </div>
+            <div class="w-[500px] h-[400px] flex justify-center items-center">
+                <img src="images/pickuptruck.png" alt="Pick Up Truck and ORCR" class="">
+            </div>
+        </div>
 
+        <div class="product2 flex flex-wrap gap-20 justify-center">
+            <div class="product1-description w-[500px] flex flex-col justify-center gap-5">
+                <h1 class="font-bold text-[#538FD6] text-5xl text-center">Sangla OR/CR</h1>
+                <p class="italic text-lg font-bold text-center">Kailangan mo ba ng agarang cash pero ayaw mong iwan ang sasakyan mo?</p>
+                <p class=" text-[14px] text-center"> Sa aming Sangla OR/CR service, puwede mong gamitin ang Official Receipt at Certificate of Registration 
+                    ng iyong sasakyan bilang collateral — at ikaw pa rin ang gagamit ng sasakyan. Mabilis ang proseso, flexible ang terms, at
+                     mababa ang interest rate. Perfect ito para sa mga biglaang pangangailangan o dagdag puhunan sa negosyo.
+                </p>
+                <button class="btn w-1/2 self-center">Apply Now</button>
+            </div>
+            <div class="w-[500px] h-[400px] flex justify-center items-center">
+                <img src="images/pickuptruck.png" alt="Pick Up Truck and ORCR" class="">
+            </div>
+        </div>
+
+        <div class="product3 flex flex-wrap gap-20 justify-center">
+            <div class="product1-description w-[500px] flex flex-col justify-center gap-5">
+                <h1 class="font-bold text-[#538FD6] text-5xl text-center">Sangla OR/CR</h1>
+                <p class="italic text-lg font-bold text-center">Kailangan mo ba ng agarang cash pero ayaw mong iwan ang sasakyan mo?</p>
+                <p class=" text-[14px] text-center"> Sa aming Sangla OR/CR service, puwede mong gamitin ang Official Receipt at Certificate of Registration 
+                    ng iyong sasakyan bilang collateral — at ikaw pa rin ang gagamit ng sasakyan. Mabilis ang proseso, flexible ang terms, at
+                     mababa ang interest rate. Perfect ito para sa mga biglaang pangangailangan o dagdag puhunan sa negosyo.
+                </p>
+                <button class="btn w-1/2 self-center">Apply Now</button>
+            </div>
+            <div class="w-[500px] h-[400px] flex justify-center items-center">
+                <img src="images/pickuptruck.png" alt="Pick Up Truck and ORCR" class="">
+            </div>
+        </div>
+
+        <div class="product4 flex flex-wrap gap-20 justify-center">
+            <div class="product1-description w-[500px] flex flex-col justify-center gap-5">
+                <h1 class="font-bold text-[#538FD6] text-5xl text-center">Sangla OR/CR</h1>
+                <p class="italic text-lg font-bold text-center">Kailangan mo ba ng agarang cash pero ayaw mong iwan ang sasakyan mo?</p>
+                <p class=" text-[14px] text-center"> Sa aming Sangla OR/CR service, puwede mong gamitin ang Official Receipt at Certificate of Registration 
+                    ng iyong sasakyan bilang collateral — at ikaw pa rin ang gagamit ng sasakyan. Mabilis ang proseso, flexible ang terms, at
+                     mababa ang interest rate. Perfect ito para sa mga biglaang pangangailangan o dagdag puhunan sa negosyo.
+                </p>
+                <button class="btn w-1/2 self-center">Apply Now</button>
+            </div>
+            <div class="w-[500px] h-[400px] flex justify-center items-center">
+                <img src="images/pickuptruck.png" alt="Pick Up Truck and ORCR" class="">
+            </div>
+        </div>
     </div>
 
 </div>
