75011e0aa9db116aa0164cef97a0652b32385b24
Keith Clarence Sison <sisonkeithclarence@gmail.com>
2025-07-29 11:58:12 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use App\Livewire\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view("index");
+    return view("livewire.index");
 });
 
 // Route::get('/index', Index::class);
