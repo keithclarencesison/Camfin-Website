5b5148c01fb7ace1322e2ac6c58eda0344d01853
Keith Clarence Sison <sisonkeithclarence@gmail.com>
2025-08-02 10:49:47 +0800

    commit changes

---
+++ resources/views/sections/corporate-identity.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <div class="header flex flex-col">
+        <h1 class="text-center text-5xl font-bold">Corporate Identity</h1>
+        <p class="w-3/4 text-center self-center">We are a purpose-driven lending company built on integrity, professionalism, and commitment. Our brand reflects trust, progress,
+            and our passion to help clients achieve financial freedom through reliable and accessible loan solutions.</p>
+    </div>
+
+    <div class="mission-vision-section flex flex-wrap justify-center gap-10 my-30">
+
+        <div class="mission-section w-[500px] bg-gray-300 flex flex-wrap justify-center items-center gap-10 max-sm:w-[400px] max-sm:p-5">
+            <div class="mission-content flex flex-col items-center w-1/2 max-sm:w-3/4">
+                <img src="images/mission-icon.png" alt="Mission Icon" class="w-[50px]">
+                <h1 class="mission-title font-bold text-3xl">Mission</h1>
+                <p class="mission-description max-sm:text-sm max-sm:w-full">To provide loan services that are Reasonable, Convenient  and Dependable.</p>
+            </div>
+            <div class="mission-img max-sm:hidden">
+                <img src="images/mission-model.png" alt="Mission Model" class="h-[250px]">
+            </div>
+        </div>
+
+        <div class="vision-section w-[500px] bg-gray-300 flex flex-wrap justify-center gap-5 max-lg:flex-row-reverse max-sm:w-[400px] max-sm:p-5">
+
+            <div class="vision-content w-1/2 flex flex-col items-center justify-center max-sm:w-3/4">
+                <img src="images/vision-icon.png" alt="Vision Icon" class="w-[50px]">
+                <h1 class="vision-title font-bold text-3xl">Vision</h1>
+                <p class="vision-description max-sm:text-sm max-sm:w-full">We are the  first choice of emerging businesses and  the marginal regions in lending services</p>
+            </div>
+            <div class="vision-img w-[200px] h-[250px] relative max-sm:hidden">
+                <img src="images/vision-model.png" alt="Vision Model" class="w-[200px] h-[250px] absolute top-0 right-0">
+            </div>
+        </div>
+    </div>
+</div>
