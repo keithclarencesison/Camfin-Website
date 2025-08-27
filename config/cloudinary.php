da1a5a966bb7e46ba89461bf28d8f0a7a2315aba
Keith Clarence Sison <sisonkeithclarence@gmail.com>
2025-08-27 11:47:23 +0800

    Update cloudinary.php

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -9,17 +9,18 @@
 
 return [
 
-    'cloud' => [
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key'        => env('CLOUDINARY_API_KEY'),
-        'api_secret'     => env('CLOUDINARY_API_SECRET'),
-    ],
-    
+    'cloud_url' => env('CLOUDINARY_URL'),
 
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME', ''),
+        'api_key'        => env('CLOUDINARY_API_KEY', ''),
+        'api_secret'     => env('CLOUDINARY_API_SECRET', ''),
+    ],
 
     'url' => [
         'secure' => true,
     ],
+
     /*
     |--------------------------------------------------------------------------
     | Cloudinary Configuration
@@ -30,8 +31,8 @@ return [
     |
     |
     */
-    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 
     /*
     |--------------------------------------------------------------------------
